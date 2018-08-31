bd3d868d3c7f9b40a0ad64d2a0f8f4d2149a7646
eryshkov <wendor265>
2018-08-31 20:45:16 +0300

    add outlets

--- Login Lab/ViewController.swift
+++ Login Lab/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK: - IBActions
+    @IBAction func forgotButtonUNTapped(_ sender: UIButton) {
+        
+    }
+    @IBAction func forgotPassButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func logInButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
