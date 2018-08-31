b68b1f9590508dc3e7366feecbe0f9a6181365d1
eryshkov <wendor265>
2018-08-31 21:25:39 +0300

    end

--- Login Lab/ViewController.swift
+++ Login Lab/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -25,14 +25,47 @@ class ViewController: UIViewController {
     
     //MARK: - IBActions
     @IBAction func forgotButtonUNTapped(_ sender: UIButton) {
-        
+        performSegue(withIdentifier: "ForgotUN", sender: nil)
     }
+    
     @IBAction func forgotPassButtonTapped(_ sender: UIButton) {
-        
+        performSegue(withIdentifier: "FogotPW", sender: nil)
     }
     
     @IBAction func logInButtonTapped(_ sender: UIButton) {
+        guard var userName = userNameTextField.text else { return }
+        
+        userName = userName.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard !userName.isEmpty else {
+            return
+        }
+        
+        performSegue(withIdentifier: "LogIn", sender: userName)
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let dvc = segue.destination as? SecondViewController else { return }
+        
+        switch segue.identifier! {
+        case "LogIn":
+            if let userName = sender as? String {
+                dvc.label.text = "Hi \(userName)."
+            }
+        case "FogotPW":
+            dvc.label.text = "Forgot password."
+        case "ForgotUN":
+            dvc.label.text = "Forgot User Name."
+        default:
+            break
+        }
+        
+    }
 }
+
+
+
+
+
+
 
